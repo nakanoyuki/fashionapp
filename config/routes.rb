4e6671abbcee7a17c83578f1e4f1024ff2b784e5
nakanoyuki <yukich41@gmail.com>
2019-10-18 23:33:00 +0900

    rails g controller cards

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
   resources :item, only: [:index, :show]
+  resources :cards ,only: [:new]
 end
