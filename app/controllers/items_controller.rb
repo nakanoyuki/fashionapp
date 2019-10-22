015bac35566eb9ec806505cbb4484f1400b03a06
nakanoyuki <yukich41@gmail.com>
2019-10-22 14:21:56 +0900

    コントローラー名を複数形に訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,4 @@
-class ItemController < ApplicationController
+class ItemsController < ApplicationController
   def index
     @items = Item.order('id ASC').limit(20)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'top#index'
-  resources :item, only: [:index, :show]
+  root 'tops#index'
+  resources :items, only: [:index, :show]
 end
