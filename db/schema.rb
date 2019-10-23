14ce8db1d4738486d126516ebfd2d70f6ace486a
nakanoyuki <yukich41@gmail.com>
2019-10-23 21:55:25 +0900

    Itemテーブルカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_121912) do
-
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "card_id"
-    t.bigint "user_id", null: false
-    t.string "customer_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_cards_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2019_10_23_125415) do
 
   create_table "item_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -35,8 +26,10 @@ ActiveRecord::Schema.define(version: 2019_10_23_121912) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "description"
-    t.integer "price", null: false
     t.string "material"
+    t.string "image"
+    t.string "color"
+    t.integer "price"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -51,7 +44,6 @@ ActiveRecord::Schema.define(version: 2019_10_23_121912) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "cards", "users"
   add_foreign_key "item_users", "items"
   add_foreign_key "item_users", "users"
 end
