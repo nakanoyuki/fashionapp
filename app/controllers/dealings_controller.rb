06eeaf1435778b0a8d269764050ef855fa1577ff
nakanoyuki <yukich41@gmail.com>
2019-10-30 01:13:20 +0900

    マイページでアイテム一覧表示

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -1,20 +1,25 @@
 class DealingsController < ApplicationController
+  before_action :create_product
 
   def create
     @item = Item.find(params[:item_id])
-    @dealings = @item.item_users.build(user_id: current_user.id)
-    @dealings.save
+    @dealings = @item.item_users.new(user_id: current_user.id)
+    begin
+      @dealings.save
+    rescue 
+      redirect_to item_path(@item) and return
+    end 
+      redirect_to root_path
   end
 
   def destroy
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
     @dealings = @item.item_users.build(user_id: current_user.id)
     @dealings.destroy if @dealings.user_id == current_user.id
   end
-
-  def show
-    
+  
+  def create_product
+    @item = Item.find(params[:item_id])
   end
-
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
- 
   def show
-    @user = User.find(current_user.id)
+    @items = current_user.items
   end
 end
