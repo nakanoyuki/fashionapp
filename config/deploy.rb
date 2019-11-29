6486dad337d5f67054b3dad86cfc87bb6c7eff7f
nakanoyuki <yukich41@gmail.com>
2019-11-29 22:29:46 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '~> 3.11.2'
+lock '3.11.2'
 
 # Capistranoのログの表示に利用する
 set :application, 'fashionapp'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:nakanoyuki/fashionapp.git'
+set :repo_url,'git@github.com:nakanoyuki/fashionapp.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
